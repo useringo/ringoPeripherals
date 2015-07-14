e5fc2ae764dfa13390de6aa49499ae25a2e6d69b
gmittal <gautam@mittal.net>
2015-07-14 16:10:47 -0700

    refactoring

---
+++ new-class-templates/YOURCLASS.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+import SpriteKit
+
+
+// this file does not have to be limited to UIViewController or the functions provided. Feel free to hack this file to your needs, such as changing the class and the main functions 
+class YOURCLASS: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
